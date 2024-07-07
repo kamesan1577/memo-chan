2085e633d3ab36d72fe2995b572385e353839b93
kamesan1577 <kamesan1577@users.noreply.github.com>
2024-07-07 15:20:57 +0900

    chore: ルートディレクトリに.envを置くようにした

--- discord_memo/config.py
+++ discord_memo/config.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
 import os
 
-load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+
+load_dotenv(os.path.join(parent_dir, ".env"))
 
 DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
