bcc0e406df3a2d0443b468283af1b912550c89a5
TOFU0218 <maikura5963@gmail.com>
2024-07-08 17:14:50 +0900

    update: #15 コマンド名を変更

--- discord_memo/cogs/create_new_tags.py
+++ discord_memo/cogs/create_new_tags.py
@@ -7,14 +7,12 @@ class CreateNewTags(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command(name='create_new_tag', description='タグを新規作成します。')
+    @app_commands.command(name='create_tag', description='タグを新規作成します。')
     async def create_new_tag(self, interaction: discord.Interaction, *, tags: str):
         custom_contents = self.bot.get_cog('CustomContents')
-        print("create_new_tag")
         tag_list = tags.split()
         tag_dict = {tag: None for tag in tag_list}
         guild = interaction.guild
-        print(tag_dict)
         # タグあるか探索
         # ある場合True
         # ない場合False
@@ -23,17 +21,10 @@ class CreateNewTags(commands.Cog):
         一旦全部False
         """
         for tag in tag_dict:
-            tag_dict[tag] = False
-        print(tag_dict)
+            tag_dict[tag] = True
         # タグすべてがある場合
-        print(all(tag_dict.values()))
-        if all(tag_dict.values()):
-            print("is True")
             if custom_contents:
                 await custom_contents.send_embed_info(interaction, 'タグリスト', 'すべてのタグが既に存在します。')
-            else:
-                print("custom_contents cog is not found")
-            return
         created_tags = {}
         for tag in tag_dict:
             if tag_dict[tag]:
