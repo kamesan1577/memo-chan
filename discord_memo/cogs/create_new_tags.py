e078d67cc33c6e405d39f75b0e77ae7cc51b8f79
TOFU-0218 <maikura5963@gmail.com>
2024-07-10 12:35:56 +0000

    feat: タグ新規作成機能

--- discord_memo/cogs/create_new_tags.py
+++ discord_memo/cogs/create_new_tags.py
@@ -1,6 +1,9 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
+from discord_memo.db.database import SessionLocal
+
+from .utils import create_tag_and_channel
 
 class CreateTags(commands.Cog):
     """タグを新規作成"""
@@ -9,30 +12,38 @@ class CreateTags(commands.Cog):
 
     @app_commands.command(name='create_tag', description='タグを新規作成します。')
     async def create_tag(self, interaction: discord.Interaction, *, tags: str):
+        print("create_tag command called with tags:", tags)
+        
         custom_contents = self.bot.get_cog('CustomContents')
         tag_list = tags.split()
-        tag_dict = {tag: None for tag in tag_list}
-        guild = interaction.guild
-        # タグあるか探索
-        # ある場合True
-        # ない場合False
-        """
-        タグ検索する処理
-        一旦全部False
-        """
-        for tag in tag_dict:
-            tag_dict[tag] = True
-        # タグすべてがある場合
+        created_tags = {}
+
+        for tag in tag_list:
+            print("Processing tag:", tag)
+            db = SessionLocal()
+            try:
+                new_channel = await create_tag_and_channel(interaction.guild, tag, db)
+                if new_channel:
+                    print("Created new channel:", new_channel.name)
+                    created_tags[tag] = new_channel.id
+                else:
+                    print("Tag already exists or failed to create channel for tag:", tag)
+            except Exception as e:
+                print("Error creating tag and channel for tag:", tag, e)
+            finally:
+                db.close()
+
+        # 全てのタグが既に存在する場合
+        if not created_tags:
+            print("All tags already exist.")
             if custom_contents:
                 await custom_contents.send_embed_info(interaction, 'タグリスト', 'すべてのタグが既に存在します。')
-        created_tags = {}
-        for tag in tag_dict:
-            if tag_dict[tag] == False:
-                new_channel = await guild.create_text_channel(tag)
-                created_tags[tag] = new_channel.id
-        # メッセージ作成
+            return
+
+        # 作成したチャンネルのメッセージを作成
         if custom_contents:
             created_channels = ' '.join([f"<#{channel_id}>" for channel_id in created_tags.values()])
+            print("Created channels:", created_channels)
             await custom_contents.send_embed_info(interaction, 'タグリスト', f'{created_channels} を作成しました。')
 
 async def setup(bot):
