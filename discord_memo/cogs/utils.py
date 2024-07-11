8521c2260bbb6f3d349ae39e238cca3201521e46
TOFU-0218 <maikura5963@gmail.com>
2024-07-11 09:10:15 +0000

    fix: #40 様々なバグを修正

--- discord_memo/cogs/utils.py
+++ discord_memo/cogs/utils.py
@@ -2,12 +2,15 @@ import re
 
 from sqlalchemy.orm import Session
 import discord
+import discord.utils
+
 from discord_memo.db.database import SessionLocal
 from discord_memo.db.crud import create_tag, get_tags_by_name, get_tag
 
 async def create_tag_and_channel(guild: discord.Guild, tag_name: list):
     """新規でタグとチャンネルを作成
     タグが存在するか確認し、存在しなかったら新規作成。
+    以下特殊ケース
     Discord側を基準とするため以下条件の場合DBに登録
     1. タグがDiscordに存在する
     2. タグがDBに存在しない
@@ -15,28 +18,35 @@ async def create_tag_and_channel(guild: discord.Guild, tag_name: list):
         guild: discord.Guild: サーバー
         tag_name: list: タグ名(<#\d+> #tag tag どの表記でも対応)
     Returns:
-        created_tags: dict: {"channel_name" : discord.TextChannel, ...}作成したチャンネル
-        new_tags: dict: {"channel_name" : discord.TextChannel, ...} 新規作成したタグ
+        created_tags: dict: {"channel_name" : discord.TextChannel, ...} : 作成したチャンネル
+        new_tags: dict: {"channel_name" : discord.TextChannel, ...} : 新規作成したタグ
     """
     created_tags = {}
     new_tags = {}
     for tag in tag_name:
         db = SessionLocal()
         try:
+            print("Processing tag:", tag)
+            # 既存タグの場合
             # <#\d+>が来た場合
             if re.match(r'<#\d+>', tag):
                 print("Processing created tag in Discord:", tag)
                 channel_id = int(tag.strip('<#>'))
                 # タグがDBに存在しない場合登録
+                print(get_tag(db, channel_id))
                 if not get_tag(db, channel_id):
                     print("Tag not found in DB, creating tag for channel:", channel_id)
                     channel_name = guild.get_channel(channel_id).name
                     create_tag(db, channel_id, channel_name)
                     print("Created tag for channel:", channel_name)
                 created_tags[tag] = guild.get_channel(channel_id)
-            # タグ新規作成の場合
+            elif get_tags_by_name(db, tag):
+                print("Processing created tag in DB:", tag)
+                tag_db = get_tags_by_name(db, tag)[0]
+                created_tags[tag_db.name] = guild.get_channel(tag_db.channel_id)
+            # タグ新規作成
             # #が最初に来る場合
-            if re.match(r'#', tag):
+            elif re.match(r'#\d+', tag):
                 print("Processing new tag:", tag)
                 # 新しいチャンネルを作成
                 # #を削除
@@ -57,4 +67,6 @@ async def create_tag_and_channel(guild: discord.Guild, tag_name: list):
             print("Error creating tag and channel for tag:", tag, e)
         finally:
             db.close()
+    print(f"created tags:{created_tags}")
+    print(f"new tags:{new_tags}")
     return created_tags, new_tags
