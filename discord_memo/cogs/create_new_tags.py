127e17ee06bbabef0c04b2cf324340876afcd259
TOFU-0218 <maikura5963@gmail.com>
2024-07-11 09:10:31 +0000

    update: #40 utils.py書き換えに伴う修正作業

--- discord_memo/cogs/create_new_tags.py
+++ discord_memo/cogs/create_new_tags.py
@@ -16,35 +16,15 @@ class CreateTags(commands.Cog):
         
         custom_contents = self.bot.get_cog('CustomContents')
         tag_list = tags.split()
-        created_tags = {}
-
-        for tag in tag_list:
-            print("Processing tag:", tag)
-            db = SessionLocal()
-            try:
-                new_channel = await create_tag_and_channel(interaction.guild, tag, db)
-                if new_channel:
-                    print("Created new channel:", new_channel.name)
-                    created_tags[tag] = new_channel.id
-                else:
-                    print("Tag already exists or failed to create channel for tag:", tag)
-            except Exception as e:
-                print("Error creating tag and channel for tag:", tag, e)
-            finally:
-                db.close()
-
-        # 全てのタグが既に存在する場合
-        if not created_tags:
-            print("All tags already exist.")
+        created_tags, new_tags = await create_tag_and_channel(interaction.guild, tag_list)
+        if new_tags:
+            new_tag = ' '.join([f"<#{channel.id}>" for channel in new_tags.values()])
             if custom_contents:
-                await custom_contents.send_embed_info(interaction, 'タグリスト', 'すべてのタグが既に存在します。')
-            return
+                await custom_contents.send_embed_info(interaction, 'タグリスト', f'{new_tag} を作成しました。')
+        else:
+            if custom_contents:
+                await custom_contents.send_embed_info(interaction, 'タグリスト', '存在するタグです。')
 
-        # 作成したチャンネルのメッセージを作成
-        if custom_contents:
-            created_channels = ' '.join([f"<#{channel_id}>" for channel_id in created_tags.values()])
-            print("Created channels:", created_channels)
-            await custom_contents.send_embed_info(interaction, 'タグリスト', f'{created_channels} を作成しました。')
 
 async def setup(bot):
     await bot.add_cog(CreateTags(bot))
