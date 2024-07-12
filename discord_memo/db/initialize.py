ffffdc884315cf118224422176e06d3c19b3fa73
kamesan1577 <kamesan1577@users.noreply.github.com>
2024-07-12 20:31:04 +0900

    chore: インポートの修正

--- discord_memo/db/initialize.py
+++ discord_memo/db/initialize.py
@@ -1,20 +1,19 @@
 from sqlalchemy.orm import Session
 from discord_memo.db.database import SessionLocal, engine
 from discord_memo.db import models, crud
-from .database import Base
+from discord_memo.db.database import Base
 
 if __name__ == "__main__":
-	Base.metadata.create_all(bind=engine)
-	crud.create_tag(SessionLocal(), channel_id=1, name="hoge")
-	crud.create_tag(SessionLocal(), channel_id=1, name="fuga")
-	print(crud.get_tags(SessionLocal()))
-	print(crud.get_tag(SessionLocal(), 1))
-	print(crud.get_tag(SessionLocal(), 2))
+    Base.metadata.create_all(bind=engine)
+    crud.create_tag(SessionLocal(), channel_id=1, name="hoge")
+    crud.create_tag(SessionLocal(), channel_id=1, name="fuga")
+    print(crud.get_tags(SessionLocal()))
+    print(crud.get_tag(SessionLocal(), 1))
+    print(crud.get_tag(SessionLocal(), 2))
 
-	crud.update_tag(SessionLocal(), 1, "piyo")
-	print(crud.get_tag(SessionLocal(), 1))
+    crud.update_tag(SessionLocal(), 1, "piyo")
+    print(crud.get_tag(SessionLocal(), 1))
 
-	crud.delete_tag(SessionLocal(), 1)
-	print(crud.get_tag(SessionLocal(), 1))
-	print(crud.get_tags(SessionLocal()))
-
+    crud.delete_tag(SessionLocal(), 1)
+    print(crud.get_tag(SessionLocal(), 1))
+    print(crud.get_tags(SessionLocal()))
