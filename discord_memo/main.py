1a9556f7f7390155e50f7d83c07f68f364e46fc3
kamesan1577 <kamesan1577@users.noreply.github.com>
2024-07-13 14:46:02 +0900

    chore: main実行時にmodelsをimportするようにした あと.ruff_cacheをignoreするように

--- discord_memo/main.py
+++ discord_memo/main.py
@@ -3,37 +3,40 @@ import discord
 import os
 from discord.ext import commands
 from discord_memo.db.database import Base, engine
+
+from discord_memo.db.models import Tag, Message, FileEntry, Tag2Message, MessageGroup
 from discord_memo import config
 
 intents = discord.Intents.default()
 
+
 async def main():
-	extensions = []
-	current_dir = os.path.dirname(os.path.abspath(__file__))
-	for cog in os.listdir(os.path.join(current_dir, "cogs")):
-		if cog.endswith(".py"):
-			extensions.append(f"cogs.{cog[:-3]}")
+    extensions = []
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    for cog in os.listdir(os.path.join(current_dir, "cogs")):
+        if cog.endswith(".py"):
+            extensions.append(f"cogs.{cog[:-3]}")
 
-	TOKEN = config.DISCORD_TOKEN
+    TOKEN = config.DISCORD_TOKEN
 
-	bot = commands.Bot(command_prefix="/", intents=intents)
+    bot = commands.Bot(command_prefix="/", intents=intents)
 
-	async def load_extensions(bot):
-		for extension in extensions:
-			try:
-				await bot.load_extension(extension)
-				print(f"Loaded extesion: {extension}")
-			except Exception as e:
-				print(f"Failed to load extension {extension}: {e}")
+    async def load_extensions(bot):
+        for extension in extensions:
+            try:
+                await bot.load_extension(extension)
+                print(f"Loaded extesion: {extension}")
+            except Exception as e:
+                print(f"Failed to load extension {extension}: {e}")
 
-	@bot.event
-	async def setup_hook() -> None:
-		await bot.tree.sync()
+    @bot.event
+    async def setup_hook() -> None:
+        await bot.tree.sync()
 
-	await load_extensions(bot)
-	await bot.start(TOKEN)
+    Base.metadata.create_all(bind=engine)
+    await load_extensions(bot)
+    await bot.start(TOKEN)
 
 
 if __name__ == "__main__":
-	Base.metadata.create_all(bind=engine)
-	asyncio.run(main())
+    asyncio.run(main())
