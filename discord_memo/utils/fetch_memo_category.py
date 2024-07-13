f31cb2647a3d84da4c59cbf95701de9a8af55c89
TOFU-0218 <maikura5963@gmail.com>
2024-07-13 13:37:45 +0000

    feat: #73 fetch_memo_categoryを作成

---
+++ discord_memo/utils/fetch_memo_category.py
@@ -0,0 +1,28 @@
+import discord
+
+category_name = "memo-bot"
+
+async def fetch_memo_category(interaction: discord.Interaction) -> discord.CategoryChannel | None:
+    """サーバー二存在するメモ用カテゴリのIDを取得
+    もしカテゴリが存在しない場合は自動で作成する
+
+    Args:
+        interaction (discord.Interaction):
+    Returns:
+        category_id (int): 取得、作成したカテゴリのID
+    """
+    print("fetch_memo_categoryu called")
+    guild = interaction.guild
+    try:
+        if not guild:
+            # エラーメッセージを送信
+            print("No guild found")
+        memo_category = discord.utils.get(guild.categories, name=category_name)
+        # カテゴリがあるなしの処理
+        if memo_category:
+            return memo_category
+        else:
+            new_category = await guild.create_category(category_name, reason="メモ用カテゴリを作成しました。")
+    except:
+        print("Error getting memo category")
+        return None
